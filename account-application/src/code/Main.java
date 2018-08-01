b94b0607849af9a94f58695fac9a92c28e419f44
Hentho <henrietta.thonnersten@academytrainee.com>
2018-08-01 09:20:02 +0100

    Initial commit

---
+++ account-application/src/code/Main.java
@@ -0,0 +1,15 @@
+package code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(getOutput());
+	}
+	
+	static public String getOutput() {
+		return "Hello world!";
+	}
+	
+	
+
+}
